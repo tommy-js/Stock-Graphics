741c7f5b8207dcb271fbe818a143dc653fc4358f
Tyler Blovat <tyler.blovat@uconn.edu>
2020-09-04 17:56:53 -0400

    First general changes to canvas.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Entrypoint for graphics engine.
 // This project will be a small graphics library, designed for the express purpose of
 // rendering stock charts and allowing for user input and interaction.
-
-console.log("hello test");
-console.log("hello again");
+var canv = document.createElement("canvas");
+canv.setAttribute("id", "canvasID");
+canv.style.border = "1px solid black";
+document.body.appendChild(canv);
