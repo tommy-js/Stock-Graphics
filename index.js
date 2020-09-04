63db0fcea77a2c99938674724f371e07febf3fe6
Tyler Blovat <tommy@MacBook-Pro.fios-router.home>
2020-09-04 17:24:55 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// Entrypoint for graphics engine.
+// This project will be a small graphics library, designed for the express purpose of
+// rendering stock charts and allowing for user input and interaction.
+
+console.log("hello test");
